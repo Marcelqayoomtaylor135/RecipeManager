f33bd635f90ed6facb4fbe19735e9e586a43514c
Marcelqayoomtaylor135 <marcelqayoomtaylor@gmail.com>
2022-02-28 15:03:43 +1100

    Fourth Commit

--- src/RecipeManagerApp.java
+++ src/RecipeManagerApp.java
@@ -19,6 +19,8 @@ public class RecipeManagerApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
+        System.out.println("test");
+        
         StorageManager sm = new StorageManager();
         
         User user = null;
